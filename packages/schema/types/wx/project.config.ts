9a2c5cce04e1c717f11273c1d8f960a294b1ad9f
ln0y <ln0y@outlook.com>
2023-06-17 16:52:27 +0800

    feat: 更新 project.config.ts 类型定义，增加 skeletonConfig 定义

--- packages/schema/types/wx/project.config.ts
+++ packages/schema/types/wx/project.config.ts
@@ -47,16 +47,16 @@ export interface ProjectConfig {
   /**
    * 编译类型
    *
-   * `"miniprogram"` 当前为普通小程序项目
+   * - `"miniprogram"` 当前为普通小程序项目
    *
-   * `"plugin"` 当前为小程序插件项目
+   * - `"plugin"` 当前为小程序插件项目
    *
    * @default "miniprogram"
    */
-  compileType: compileType
+  compileType: compileType | string
 
   /**
-   * 项目设置
+   * 项目的编译设置，可以指定以下设置。部分设置无法在 `project.private.config.json` 中生效
    */
   setting?: setting
 
@@ -65,14 +65,14 @@ export interface ProjectConfig {
    *
    * 可以指定项目运行的基础库具体的版本号，也可以指定以下值
    *
-   * `"latest"` 最新的非灰度中的基础库
+   * - `"latest"` 最新的非灰度中的基础库
    *
-   * `"trial"` 最新的基础库
+   * - `"trial"` 最新的基础库
    *
-   * `"widelyUsed"` 使用比例最高的基础库
+   * - `"widelyUsed"` 使用比例最高的基础库
    *
    */
-  libVersion?: libVersion
+  libVersion?: libVersion | string
 
   /**
    * 项目的 appid，`只在新建项目时读取`
@@ -132,8 +132,11 @@ export interface ProjectConfig {
   staticServerOptions?: staticServerOptions
 
   /**
+   * 编辑器设置 （会覆盖工具里的设置）
+   *
    * 指定自动生成的文件的 tabIndent 和 tabSize
    *
+   * @examples require('.').editorSettingExample
    */
   editorSetting?: editorSetting
 
@@ -154,23 +157,48 @@ export interface ProjectConfig {
    * 模拟器的版本
    */
   simulatorPluginLibVersion?: {}
+
+  /**
+   * 骨架屏配置 [点击查看文档](https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html)
+   */
+  skeletonConfig?: skeletonConfig
+
+  /**
+   *
+   */
+  jsserverRoot?: string
+
+  /**
+   *
+   */
+  pluginAppid?: string
+
+  /**
+   *
+   */
+  srcMiniprogramRoot?: string
+
+  [key: string]: any
 }
 
 /**
  * 编译类型作用于开发工具的以下设置
- *
- * `"miniprogram"` 当前为普通小程序项目
- *
- * `"plugin"` 当前为小程序插件项目
  */
-export type compileType = 'miniprogram' | 'plugin'
+export enum compileType {
+  /**
+   * 当前为小程序插件项目
+   */
+  plugin = 'plugin',
 
-/**
- * 项目的编译设置，可以指定以下设置。部分设置无法在 `project.private.config.json` 中生效
- */
+  /**
+   * 当前为普通小程序项目
+   */
+  miniprogram = 'miniprogram',
+}
+
 export interface setting {
   /**
-   * 启用[条件编译](https://dev.weixin.qq.com/docs/framework/dev/framework/operation/condition-compile.html)
+   * 启用条件编译 [点击查看文档](https://dev.weixin.qq.com/docs/framework/dev/framework/operation/condition-compile.html)
    *
    * 允许私有设置 否
    */
@@ -281,7 +309,7 @@ export interface setting {
    *
    * 允许私有设置 是
    *
-   * 注: (懒注入占位组件调试) 开启 [按需注入](https://developers.weixin.qq.com/miniprogram/dev/framework/ability/lazyload.html)
+   * 注: (懒注入占位组件调试) 开启按需注入 [点击查看文档](https://developers.weixin.qq.com/miniprogram/dev/framework/ability/lazyload.html)
    * 后在项目设置面板中会出现对应的勾选项，
    * 开启之后，按需注入的组件将会停止注入，页面停止在占位组件状态，
    * 便于调试自定义占位组件。
@@ -361,7 +389,7 @@ export interface setting {
   packNpmManually?: boolean
 
   /**
-   * 仅 packNpmManually 为 true 时生效，详细参考 [构建 npm 文档](https://developers.weixin.qq.com/miniprogram/dev/devtools/npm.html)
+   * 仅 packNpmManually 为 true 时生效，详细参考构建 npm 文档 [点击查看文档](https://developers.weixin.qq.com/miniprogram/dev/devtools/npm.html)
    *
    * 允许私有设置 否
    *
@@ -479,39 +507,38 @@ export interface setting {
   nodeModules?: boolean
 
   /**
-   * 新特性，文档中未描述
+   * 新特性
    */
   newFeature?: boolean
 
   /**
-   * 文档中未描述
+   *
    */
   bundle?: boolean
 
   /**
-   * 文档中未描述
+   *
    */
   scopeDataCheck?: boolean
 
   /**
-   * 使用新的编译模块，文档中未描述
+   * 使用新的编译模块
    */
   useCompilerModule?: boolean
 
   /**
-   * 文档中未描述
+   *
    */
   userConfirmedBundleSwitch?: boolean
 
   /**
-   * 文档中未描述
+   *
    */
   userConfirmedUseCompilerModuleSwitch?: boolean
+
+  [key: string]: any
 }
 
-/**
- * npm 包关系列表
- */
 export interface packNpmRelationList {
   /**
    * `packageJsonPath` 表示 node_modules 源对应的 package.json
@@ -522,6 +549,8 @@ export interface packNpmRelationList {
    * miniprogramNpmDistDir 表示 node_modules 的构建结果目标位置
    */
   miniprogramNpmDistDir: string
+
+  [key: string]: any
 }
 
 export const packNpmRelationListExample: packNpmRelationList[][] = [
@@ -535,9 +564,6 @@ export const packNpmRelationListExample: packNpmRelationList[][] = [
 
 export type useCompilerPlugins = 'typescript' | 'less' | 'sass'
 
-/**
- * `将 JS 编译为 ES5` 时 Babel 的配置项，其中可以指定以下设置
- */
 export interface babelSetting {
   /**
    * Babel 辅助函数的输出目录，默认为 `@babel/runtime`
@@ -555,23 +581,27 @@ export interface babelSetting {
    * 关闭的插件
    */
   disablePlugins?: string[]
+
+  [key: string]: any
 }
 
-/**
- * 空接口
- *
- * https://github.com/microsoft/TypeScript/issues/29729#issuecomment-1483854699
- *
- * @TJS-type string
- */
-interface Nothing {}
-type LiteralUnion<T, U = string> = T | (U & Nothing)
+export enum libVersion {
+  /**
+   * 最新的非灰度中的基础库
+   */
+  latest = 'latest',
 
-export type libVersion = LiteralUnion<'latest' | 'trial' | 'widelyUsed'>
+  /**
+   * 最新的基础库
+   */
+  trial = 'trial',
 
-/**
- * packOptions 用以配置项目在打包过程中的选项。打包是预览、上传时对项目进行的必须步骤。
- */
+  /**
+   * 使用比例最高的基础库
+   */
+  widelyUsed = 'widelyUsed',
+}
+
 export interface packOptions {
   /**
    * 用以配置打包时需要强制带上的文件（仅限后缀名白名单内）或者文件夹，
@@ -589,6 +619,8 @@ export interface packOptions {
    * @examples require('.').packItemExample
    */
   ignore?: packItem[]
+
+  [key: string]: any
 }
 
 export interface packItem {
@@ -608,7 +640,41 @@ export interface packItem {
    *
    * 注: `regexp`、`glob` 仅 1.02.1809260 及以上版本工具支持。
    */
-  type: packItemType
+  type: packItemType | string
+
+  [key: string]: any
+}
+
+export enum packItemType {
+  /**
+   * 文件夹
+   */
+  folder = 'folder',
+
+  /**
+   * 文件
+   */
+  file = 'file',
+
+  /**
+   * 后缀
+   */
+  suffix = 'suffix',
+
+  /**
+   * 前缀
+   */
+  prefix = 'prefix',
+
+  /**
+   * 正则表达式
+   */
+  regexp = 'regexp',
+
+  /**
+   * Glob 规则
+   */
+  glob = 'glob',
 }
 
 export const packItemExample: packItem[][] = [
@@ -650,17 +716,6 @@ export const packItemExample: packItem[][] = [
   ],
 ]
 
-export type packItemType =
-  | 'folder'
-  | 'file'
-  | 'suffix'
-  | 'prefix'
-  | 'regexp'
-  | 'glob'
-
-/**
- * debugOptions 用以配置在对项目代码进行调试时的选项。
- */
 export interface debugOptions {
   /**
    * 用以配置调试时于调试器 Sources 面板隐藏源代码的文件。
@@ -673,18 +728,10 @@ export interface debugOptions {
    * @examples require('.').packItemExample
    */
   hidedInDevtools?: packItem[]
+
+  [key: string]: any
 }
 
-/**
- * 文件监听配置设置
- *
- * `watchOptions` 用以配置项目中可以被忽略展示和监听文件变化的文件匹配规则。
- * 部分项目（如游戏项目）其项目文件可能成千上万，其中大部分可能是资源文件，
- * 并不是小程序和小游戏代码中关心展示的文件，这些文件在工具开发时可以不需要被开发者关注，
- * 因此工具可以增加一个配置去忽略指定的文件和目录，
- * 从而不对这些文件进行文件遍历获取和文件内容变更的监听（watch），
- * 从而提高开启工具的速度以及减少工具打开占用的内存。
- */
 export interface watchOptions {
   /**
    * `ignore`用以配置工具时对符合指定规则的文件或文件夹进行忽略
@@ -706,6 +753,8 @@ export interface watchOptions {
    * @examples require('.').ignoreFileExamples
    */
   ignore?: string[]
+
+  [key: string]: any
 }
 
 export const ignoreFileExamples: string[][] = [
@@ -713,9 +762,6 @@ export const ignoreFileExamples: string[][] = [
   ['libs/**/**'],
 ]
 
-/**
- * 指定自定义预处理的命令
- */
 export interface scripts {
   /**
    * 编译前预处理命令
@@ -731,13 +777,10 @@ export interface scripts {
    * 上传前预处理命令
    */
   beforeUpload?: string
+
+  [key: string]: any
 }
 
-/**
- * 仅在小游戏项目中有效
- *
- * 在微信开发者工具里开启一个本地静态资源服务器，用于托管项目静态资源。
- */
 export interface staticServerOptions {
   /**
    * 可以指向项目路径下的某个目录作为静态资源路径。
@@ -745,27 +788,22 @@ export interface staticServerOptions {
    * 通常可以用于上线发布前预览测试静态资源加载效果。
    */
   servePath?: string
+
+  [key: string]: any
 }
 
-/**
- * 编辑器设置 （会覆盖工具里的设置）
- *
- * 指定自动生成的文件的 tabIndent 和 tabSize
- *
- * @examples require('.').editorSettingExample
- */
 export interface editorSetting {
   /**
    * tab 缩进
    *
-   * `"auto"` 自动检测缩进
+   * - `"auto"` 自动检测缩进
    *
-   * `"insertSpaces"` 用空格代替 Tab
+   * - `"insertSpaces"` 用空格代替 Tab
    *
-   * `"tab"` 使用 Tab
+   * - `"tab"` 使用 Tab
    *
    */
-  tabIndent?: 'auto' | 'insertSpaces' | 'tab'
+  tabIndent?: tabIndent | string
 
   /**
    * tab 大小
@@ -774,7 +812,26 @@ export interface editorSetting {
    *
    * @default 4
    */
-  tabSize?: number
+  tabSize?: number | string | '\t'
+
+  [key: string]: any
+}
+
+export enum tabIndent {
+  /**
+   * 自动检测缩进
+   */
+  auto = 'auto',
+
+  /**
+   * 用空格代替 Tab
+   */
+  insertSpaces = 'insertSpaces',
+
+  /**
+   * 使用 Tab
+   */
+  tab = 'tab',
 }
 
 export const editorSettingExample: editorSetting[] = [{
@@ -783,34 +840,33 @@ export const editorSettingExample: editorSetting[] = [{
 }]
 
 export interface condition {
-  search?: partialCompileList
-  conversation?: partialCompileList
+  search?: customCompileList
+  conversation?: customCompileList
 
   /**
    * 小游戏模式
    */
-  game?: partialCompileList
+  game?: customCompileList<gameCustomCompile>
 
   /**
    * 小游戏插件模式
    */
-  gamePlugin?: partialCompileList
+  gamePlugin?: customCompileList
 
   /**
    * 插件模式
    */
-  plugin?: partialCompileList
+  plugin?: customCompileList<pluginCustomCompile>
 
   /**
    * 小程序模式
    */
-  miniprogram?: partialCompileList
+  miniprogram?: customCompileList<miniprogramCustomCompile>
+
+  [key: string]: any
 }
 
-/**
- * 局部编译
- */
-export interface partialCompileList {
+export type customCompileList<T = baseCustomCompile> = {
   /**
    *
    * @TJS-type integer
@@ -822,29 +878,25 @@ export interface partialCompileList {
    *
    * <https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html>
    *
-   * @examples require('.').partialCompileExample
    */
-  list?: partialCompile[]
+  list?: T[]
+
+  [key: string]: any
 }
 
-export interface partialCompile {
+interface baseCustomCompile {
   /**
-   * 模式名称 [详情](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   * 模式名称 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
    */
   name: string
 
   /**
-   * 启动页面路径 [详情](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
-   */
-  pathName: string
-
-  /**
-   * 启动参数 [详情](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   * 启动参数 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
    */
   query: string
 
   /**
-   * 进入场景 [详情](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   * 进入场景 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
    */
   scene: string | number | null
 
@@ -853,26 +905,330 @@ export interface partialCompile {
   referrerInfo?: {}
 
   groupInfo?: {}
+
+  [key: string]: any
 }
 
-export const partialCompileExample: partialCompile[][] = [
-  [
-    {
-      name: '登录',
-      pathName: 'pages/login/index',
-      query: 'name=vendor&color=black',
-      scene: null,
-    },
-  ],
-  [
-    {
-      name: '首页',
-      pathName: 'pages/index/index',
-      query: 'scene=666',
-      scene: null,
-    },
-  ],
-]
+export interface gameCustomCompile extends baseCustomCompile {
+
+  /**
+   * 启动页面路径 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   */
+  pathName: string
+
+  chatroomUsernameInfo?: {}
+}
+
+export interface pluginCustomCompile extends baseCustomCompile {
+
+  /**
+   * 启动页面路径 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   */
+  pathName: string
+
+  /**
+   * 小程序模式（启动模式）
+   *
+   *
+   */
+  launchMode?: launchMode | string
+}
+
+export interface miniprogramCustomCompile extends baseCustomCompile {
+
+  /**
+   * 启动页面路径 [点击查看文档](https://developers.weixin.qq.com/minigame/dev/devtools/partial-compile.html)
+   */
+  pathName: string
+
+  launchMode?: launchMode | string
+
+  /**
+   * 局部编译
+   */
+  partialCompile?: partialCompile
+
+  chatroomUsernameInfo?: {}
+}
+
+export enum launchMode {
+  /**
+   * 默认
+   */
+  default = 'default',
+
+  /**
+   * 全屏类原生（商品橱窗等场景）
+   */
+  defaultNativeFunctionalized = 'defaultNativeFunctionalized',
+
+  /**
+   * 半屏类原生（直播商品等场景）
+   */
+  halfPageNativeFunctionalized = 'halfPageNativeFunctionalized',
+
+  /**
+   * 快照页（朋友圈等场景）
+   */
+  singlePage = 'singlePage',
+
+  /**
+   * 半屏（openEmbeddedMiniProgram打开）
+   */
+  embedded = 'embedded',
+}
+
+export interface partialCompile {
+  /**
+   * 是否开启局部编译
+   */
+  enabled: boolean
+
+  /**
+   * 局部编译的页面
+   */
+  pages: string[]
+
+  [key: string]: any
+}
+
+// export const partialCompileExample: partialCompile[][] = [
+//   [
+//     {
+//       name: '登录',
+//       pathName: 'pages/login/index',
+//       query: 'name=vendor&color=black',
+//       scene: null,
+//     },
+//   ],
+//   [
+//     {
+//       name: '首页',
+//       pathName: 'pages/index/index',
+//       query: 'scene=666',
+//       scene: null,
+//     },
+//   ],
+// ]
+
+export interface skeletonConfig {
+  /**
+   * 全局配置
+   */
+  global: ISkeletonConfig
+  pages?: {
+    [key: string]: ISkeletonConfig
+  }
+}
+
+export interface ISkeletonConfig {
+  /**
+   * 骨架屏显示时的动画
+   *
+   * - `"spin"`
+   *
+   * - `"chiaroscuro"`
+   *
+   * - `"shine"`
+   *
+   * @default "spin"
+   */
+  loading?: loading | string
+
+  /**
+   * 骨架页面中的边框
+   */
+  outline?: {
+    /**
+     * 保持边框颜色，默认为 true
+     *
+     * @default true
+     */
+    remain: boolean
+
+    /**
+     * `remain` 为 `false` 时，用 `replace` 字段替代 style.border
+     */
+    replace: string
+
+    [key: string]: any
+  }
+
+  /**
+   * 骨架页面中的文字块
+   */
+  text?: {
+    /**
+     * 用于决定骨架页面中文字块的的颜色，颜色值支持 16 进制
+     */
+    color: string
+
+    [key: string]: any
+  }
+
+  /**
+   * 骨架页面中的图片块
+   */
+  image?: {
+    /**
+     * 用于确定骨架页面中图片块的颜色，颜色值支持 16 进制，
+     */
+    color: string
+
+    /**
+     * 用于确定骨架页面中图片块的形状，形状支持两个枚举值
+     *
+     * - `"circle"` （圆形）
+     *
+     * - `"rect"` （矩形）。
+     */
+    shape: shape | string
+
+    /**
+     * shapeOpposite 字段接受一个数组，数组中每个元素是一个 DOM 选择器，
+     * 用于选择 DOM 元素，被选择 DOM 的形状将和配置的 shape 形状相反，
+     * 例如，配置的是 `rect` 那么，shapeOpposite 中的图片块将在骨架页面中显示成 circle 形状（圆形）
+     */
+    shapeOpposite: string[]
+
+    [key: string]: any
+  }
+
+  /**
+   * 骨架页面中的按钮块
+   */
+  button?: {
+    /**
+     * 用于确定骨架页面中按钮块的颜色，颜色值支持 16 进制，
+     */
+    color: string
+
+    /**
+     * `excludes` 接受一个数组，数组中元素是 DOM 选择器，
+     * 用来选择元素，该数组中的元素将不被视为按钮块。
+     */
+    excludes: string[]
+
+    [key: string]: any
+  }
+
+  /**
+   * 骨架页面中伪元素块
+   */
+  pseudo?: {
+    /**
+     * 用于确定骨架页面中伪元素块的颜色，颜色值支持 16 进制，
+     */
+    color: string
+
+    /**
+     * 用于确定骨架页面中伪元素块的形状，形状支持两个枚举值
+     *
+     * - `"circle"` （圆形）
+     *
+     * - `"rect"` （矩形）。
+     */
+    shape: shape | string
+
+    [key: string]: any
+  }
+
+  /**
+   * 如果你有不需要进行骨架处理的元素，那么将该元素的 CSS 选择器写入该数组。
+   */
+  excludes?: string[]
+
+  /**
+   * 不需要生成页面骨架，且需要从 DOM 中移除的元素，配置值为移除元素的 CSS 选择器。
+   */
+  remove?: string[]
+
+  /**
+   * 不需要移除，但是通过设置其透明度为 0，来隐藏该元素，配置值为隐藏元素的 CSS 选择器。
+   */
+  hide?: string[]
+
+  /**
+   * 该数组中元素是 CSS 选择器，被选择的元素将被清空子元素。
+   */
+  empty?: string[]
+
+  /**
+   * 该数组中元素是 CSS 选择器，被选择的元素将被被插件处理成一个色块，
+   * 色块的颜色和按钮块颜色一致。内部元素将不再做特殊处理，文字将隐藏。
+   */
+  grayBlock?: string[]
+
+  /**
+   * 显示原生组件，为 `false` 时原生组件被处理为 view。
+   *
+   * @default false
+   */
+  showNative?: boolean
+
+  /**
+   * 骨架屏背景色
+   *
+   * @default "transparent"
+   */
+  backgroundColor?: string
+
+  /**
+   * 默认为使用绝对定位占满全屏。当对自定义组件使用，
+   * 作为局部加载的样式时，可设为 `auto` ，高度随内容撑开
+   *
+   * @default "fullscreen"
+   */
+  mode?: mode | string
+
+  /**
+   * 骨架屏模板的 name 值
+   *
+   * @default "skeleton"
+   */
+  templateName?: string
+
+  /**
+   * 支持的枚举值： `"rpx"`, `"rem"`, `"vw"`, `"vh"`, `"vmin"`, `"vmax"`
+   *
+   * @default "rpx"
+   */
+  cssUnit?: cssUnit | string
+
+  /**
+   * 生成骨架页面中 css 值保留的小数位数，默认值是 4
+   *
+   * @default 4
+   */
+  decimal?: number
+
+  [key: string]: any
+}
+
+export enum loading {
+  chiaroscuro = 'chiaroscuro',
+  shine = 'shine',
+  spin = 'spin',
+}
+
+export enum shape {
+  circle = 'circle',
+  rect = 'rect',
+}
+
+export enum mode {
+  auto = 'auto',
+  fullscreen = 'fullscreen',
+}
+
+export enum cssUnit {
+  rem = 'rem',
+  rpx = 'rpx',
+  vh = 'vh',
+  vmax = 'vmax',
+  vmin = 'vmin',
+  vw = 'vw',
+}
 
 // todo 添加 "markdownDescription"
 // https://github.com/microsoft/vscode/issues/34498
